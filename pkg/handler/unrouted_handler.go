1e555d825548f0375a3abef71ebdc654dd779818
Amir So <amir.so@icloud.com>
2020-09-14 11:52:09 +0200

    Force getHostAndProtocol to use https

--- pkg/handler/unrouted_handler.go
+++ pkg/handler/unrouted_handler.go
@@ -979,6 +979,9 @@ func (handler *UnroutedHandler) absFileURL(r *http.Request, id string) string {
 
 	// Read origin and protocol from request
 	host, proto := getHostAndProtocol(r, handler.config.RespectForwardedHeaders)
+	if proto == "http" {
+		proto = "https"
+	}
 
 	url := proto + "://" + host + handler.basePath + id
 
